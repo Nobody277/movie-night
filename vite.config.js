24f01e15941216037d15f04d2f24cede58848d7e
Nobody277 <nobody.272.72@gmail.com>
2025-09-26 17:12:54 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        movies: resolve(process.cwd(), 'movies.html')
+      }
+    }
+  },
   server: {
     open: true
   }
